bd9580906f6048daf620ae0f077bca4d7d779aec
Tom Wu <tom76kimo@gmail.com>
2016-07-12 22:56:56 +0800

    refine port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express')
 
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port'));
 });
